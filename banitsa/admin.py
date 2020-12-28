11b6b2bccaace4df9a54e5c5f59e68b4f58cd3cd
Boyana Norris <brnorris03@gmail.com>
2020-12-28 15:37:23 -0800

    db editing admin extension (for easy editing of fortunes)

--- banitsa/admin.py
+++ banitsa/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Fortune
 
-admin.site.register(Fortune)
+class FortuneAdmin(admin.ModelAdmin):
+    list_display = ['id','fortune_text', 'english_text', 'pub_date']
+
+
+admin.site.register(Fortune, FortuneAdmin)
